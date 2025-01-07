0340985f6b8f4f83d28bb25216c9101d2b4394f7
Akai <jusup.munanbekuulu@gmail.com>
2025-01-07 22:03:26 +0600

    update

---
+++ src/utils/lbHelpers.ts
@@ -0,0 +1,3 @@
+// Этот код помогает для автоматической генерации типов для TS
+// yarn add/npm i : pg-to-ts
+// npx pg-to-ts generate -c postgres://postgres:2000@localhost:5432/expressDB -o ./src/types/dbTypes.ts -s public
